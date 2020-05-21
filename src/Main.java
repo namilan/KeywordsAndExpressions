6129985d1a373d933c6efd6765aecf20e19e9b4b
namilan <nichole.milan@gmail.com>
2020-05-21 14:54:40 -0400

    What are expressions

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,23 @@ public class Main {
 	
 	public static void main (String[] args) {
 		
-		// keywords & expressions
+		//a mile is equal to 1.609344 kilometers
+		
+		double kilometers = (100*1.609344);
+		
+		int highScore = 50;
+		if (highScore == 50) {
+			System.out.println("This is an expression");
+			
+			// in the following code that i will type below, white down what parts of the code are expressions. 
+			
+			int score = 100; // score = 100 expression
+			if (score>99)  { // score>99 expression
+				System.out.println("You got the high score!"); //you got the high score expression
+				score = 0;
+			}
+			
+		}
 	}
 
 }
